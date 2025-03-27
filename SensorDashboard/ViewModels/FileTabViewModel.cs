7562f49ab2ee18ad1a16cf831f77573877bbde10
AndrewToplass <160808664+AndrewToplass@users.noreply.github.com>
2025-03-27 21:34:39 +0800

    Add tab search and goto functionality.

--- SensorDashboard/ViewModels/FileTabViewModel.cs
+++ SensorDashboard/ViewModels/FileTabViewModel.cs
@@ -11,8 +11,16 @@ public partial class FileTabViewModel : ViewModelBase
     [ObservableProperty]
     private double _thresholdMinimum = 20;
 
-    [ObservableProperty]
-    private double _thresholdMaximum = 100;
+    [ObservableProperty] private string _title = "Tab title";
+
+    [ObservableProperty] private double _thresholdMinimum = 0;
+
+    [ObservableProperty] private double _thresholdMaximum = 100;
 
     public required SensorData SensorData { get; set; }
+
+    public override string ToString()
+    {
+        return Title;
+    }
 }
