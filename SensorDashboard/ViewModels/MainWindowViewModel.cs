1889a11fae7d0da5d0c1fcaa55097175f8cf0bc4
AndrewToplass <160808664+AndrewToplass@users.noreply.github.com>
2025-03-27 23:19:10 +0800

    Improve tab search functionality.

--- SensorDashboard/ViewModels/MainWindowViewModel.cs
+++ SensorDashboard/ViewModels/MainWindowViewModel.cs
@@ -1,44 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Collections.Specialized;
-using System.Diagnostics;
+﻿using System.Collections.ObjectModel;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using Avalonia;
 using CommunityToolkit.Mvvm.ComponentModel;
-using SensorDashboard.Models;
 
 namespace SensorDashboard.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public MainWindowViewModel()
-    {
-        // OpenTabs.CollectionChanged += OpenTabs_CollectionChanged;
-        GetOpenTabsAsync = GetOpenTabsAsyncFunc;
-    }
-
     public ObservableCollection<FileTabViewModel> OpenTabs { get; set; } = [];
 
     [ObservableProperty] private FileTabViewModel? _selectedTab;
 
     [ObservableProperty] private FileTabViewModel? _tabSearchTarget;
-
-    public Func<string?, CancellationToken, Task<IEnumerable<object>>> GetOpenTabsAsync { get; }
-
-    public Task<IEnumerable<object>> GetOpenTabsAsyncFunc(string? searchText, CancellationToken cancellationToken)
-    {
-        if (string.IsNullOrWhiteSpace(searchText))
-        {
-            return Task.FromResult<IEnumerable<object>>([]);
-        }
-
-        return Task.FromResult<IEnumerable<object>>(
-            OpenTabs
-                .Where(t => t.Title.Contains(searchText, StringComparison.OrdinalIgnoreCase)));
-    }
 
     public void GotoTab(FileTabViewModel? tab)
     {
@@ -56,6 +28,7 @@ public partial class MainWindowViewModel : ViewModelBase
         string title;
         while (true)
         {
+            // Find next unique number from untitled open tabs.
             title = $"Untitled {index}";
 
             if (OpenTabs.All(t => t.Title != title))
@@ -74,35 +47,5 @@ public partial class MainWindowViewModel : ViewModelBase
 
         OpenTabs.Add(tab);
         SelectedTab = tab;
-    }
-
-    public void OpenFile()
-    {
-        OpenTabs.Add(
-            new FileTabViewModel
-            {
-                Title = $"Sensor Dashboard {OpenTabs.Count + 1}",
-                SensorData = new()
-                {
-                    Data = new double[0, 0]
-                }
-            });
-
-        SelectedTab = OpenTabs[^1];
-    }
-
-    public void SaveFile()
-    {
-        Console.WriteLine(SelectedTab?.Title ?? "(null)");
-    }
-
-    public void CloseFile()
-    {
-        if (SelectedTab is null)
-        {
-            return;
-        }
-
-        OpenTabs.Remove(SelectedTab);
     }
 }
