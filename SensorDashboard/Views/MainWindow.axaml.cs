d2df1a794520c89b0a97fcf78a5c67dd8fba9aa5
SMTAFE-AndrewToplass <160808664+SMTAFE-AndrewToplass@users.noreply.github.com>
2025-03-27 18:39:19 +0800

    Disable Windows window border adjustment.

--- SensorDashboard/Views/MainWindow.axaml.cs
+++ SensorDashboard/Views/MainWindow.axaml.cs
@@ -33,11 +33,12 @@ public partial class MainWindow : Window
 
         RegisterHotKeys(AppMenu);
 
-        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        {
-            UpdateWin32Border(WindowState);
-            this.GetObservable(WindowStateProperty)
-                .Subscribe(UpdateWin32Border);
+        //if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        //{
+        //    UpdateWin32Border(WindowState);
+        //    this.GetObservable(WindowStateProperty)
+        //        .Subscribe(UpdateWin32Border);
+        //}
         }
     }
 
@@ -73,39 +74,39 @@ public partial class MainWindow : Window
         }
     }
 
-    [StructLayout(LayoutKind.Sequential, Pack = 1)]
-    struct MARGINS
-    {
-        public int cxLeftWidth;
-        public int cxRightWidth;
-        public int cxTopHeight;
-        public int cxBottomHeight;
-    }
+    //[StructLayout(LayoutKind.Sequential, Pack = 1)]
+    //struct MARGINS
+    //{
+    //    public int cxLeftWidth;
+    //    public int cxRightWidth;
+    //    public int cxTopHeight;
+    //    public int cxBottomHeight;
+    //}
 
-    [DllImport("dwmapi.dll")]
-    static extern int DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS pMargins);
+    //[DllImport("dwmapi.dll")]
+    //static extern int DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS pMargins);
 
-    void UpdateWin32Border(WindowState state)
-    {
-        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        {
-            return;
-        }
+    //void UpdateWin32Border(WindowState state)
+    //{
+    //    if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+    //    {
+    //        return;
+    //    }
 
-        var handle = TryGetPlatformHandle();
-        if (handle is null)
-        {
-            return;
-        }
+    //    var handle = TryGetPlatformHandle();
+    //    if (handle is null)
+    //    {
+    //        return;
+    //    }
 
-        MARGINS margins = new();
-        if (state is not WindowState.Maximized or WindowState.FullScreen)
-        {
-            margins.cxLeftWidth = 1;
-            margins.cxRightWidth = 1;
-            margins.cxTopHeight = 1;
-            margins.cxBottomHeight = 1;
-        }
-        _ = DwmExtendFrameIntoClientArea(handle.Handle, ref margins);
-    }
+    //    MARGINS margins = new();
+    //    if (state is not WindowState.Maximized or WindowState.FullScreen)
+    //    {
+    //        margins.cxLeftWidth = 1;
+    //        margins.cxRightWidth = 1;
+    //        margins.cxTopHeight = 1;
+    //        margins.cxBottomHeight = 1;
+    //    }
+    //    _ = DwmExtendFrameIntoClientArea(handle.Handle, ref margins);
+    //}
 }
