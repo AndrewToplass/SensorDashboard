41e0ff272bbf7b6a431041541b3094ca9ce0f5bb
AndrewToplass <160808664+SMTAFE-AndrewToplass@users.noreply.github.com>
2025-03-27 11:40:59 +0800

    Add window shadow on Windows 10.

--- SensorDashboard/Views/MainWindow.axaml.cs
+++ SensorDashboard/Views/MainWindow.axaml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Specialized;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
@@ -9,6 +10,8 @@ using Avalonia.LogicalTree;
 using CommunityToolkit.Mvvm.Input;
 using FluentAvalonia.UI.Controls;
 using SensorDashboard.ViewModels;
+using Avalonia;
+using System.Reactive;
 
 namespace SensorDashboard.Views;
 
@@ -21,7 +24,7 @@ public partial class MainWindow : Window
         InitializeComponent();
     }
 
-    protected override void OnLoaded(RoutedEventArgs e)
+    protected override async void OnLoaded(RoutedEventArgs e)
     {
         if (DataContext is MainWindowViewModel viewModel)
         {
@@ -29,6 +32,22 @@ public partial class MainWindow : Window
         }
 
         RegisterHotKeys(AppMenu);
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            UpdateWin32Border(WindowState);
+            this.GetObservable(WindowStateProperty)
+                .Subscribe(UpdateWin32Border);
+        }
+
+        ContentDialog dialog = new()
+        {
+            Title = "Example title",
+            Content = "Message",
+            CloseButtonText = "OK",
+        };
+
+        await dialog.ShowAsync();
     }
 
     /// <summary>
@@ -55,16 +74,48 @@ public partial class MainWindow : Window
         }
     }
 
-    /// <summary>
-    /// Click event for the close tab button.
-    /// </summary>
     private void Tabs_OnTabCloseRequested(TabView sender, TabViewTabCloseRequestedEventArgs args)
     {
-        if (args.Item is not FileTabViewModel file)
+        if (args.Item is FileTabViewModel file)
+        {
+            _viewModel.OpenTabs.Remove(file);
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    struct MARGINS
+    {
+        public int cxLeftWidth;
+        public int cxRightWidth;
+        public int cxTopHeight;
+        public int cxBottomHeight;
+    }
+
+    [DllImport("dwmapi.dll")]
+    static extern int DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS pMargins);
+
+    void UpdateWin32Border(WindowState state)
+    {
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
         {
             return;
         }
 
-        _viewModel.OpenTabs.Remove(file);
+        var handle = TryGetPlatformHandle();
+        if (handle is null)
+        {
+            return;
+        }
+
+
+        MARGINS margins = new();
+        if (state is not WindowState.Maximized or WindowState.FullScreen)
+        {
+            margins.cxLeftWidth = 1;
+            margins.cxRightWidth = 1;
+            margins.cxTopHeight = 1;
+            margins.cxBottomHeight = 1;
+        }
+        _ = DwmExtendFrameIntoClientArea(handle.Handle, ref margins);
     }
 }
