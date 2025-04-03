1f0e1eef4cc5c349c6410a16887fa4687aad2ae8
AndrewToplass <160808664+SMTAFE-AndrewToplass@users.noreply.github.com>
2025-04-03 12:29:48 +0800

    Make data processor observable,

--- SensorDashboard/Models/DataProcessor.cs
+++ SensorDashboard/Models/DataProcessor.cs
@@ -1,8 +1,9 @@
+using CommunityToolkit.Mvvm.ComponentModel;
 using System.Linq;
 
 namespace SensorDashboard.Models;
 
-public class DataProcessor
+public partial class DataProcessor : ObservableObject
 {
     public static DataProcessor Instance { get; } = new();
 
@@ -10,38 +11,12 @@ public class DataProcessor
     {
     }
 
+    [ObservableProperty] private SortedObservableCollection<SensorData> _datasets = [];
+
     public double Average(SensorData data) =>
         Enumerable.Range(0, data.Rows)
             .SelectMany(data.GetRow)
             .Average();
 
     public double AverageOfRow(SensorData data, int row) => data.GetRow(row).Average();
-
-    public int BinarySearch(double[] data, double target)
-    {
-        const double delta = 0.001;
-        var low = 0;
-        var high = data.Length - 1;
-
-        while (low <= high)
-        {
-            var mid = (low + high) / 2;
-
-            if (target >= mid - delta || target <= mid + delta)
-            {
-                return mid;
-            }
-
-            if (target < data[mid])
-            {
-                high = mid - 1;
-            }
-            else
-            {
-                low = mid + 1;
-            }
-        }
-
-        return -1;
-    }
 }
